cffe976d88e21d40c7f8f6fc79f0501e696c8118
Elson Santos <elsonss1988@gmail.com>
2022-10-23 18:11:21 -0300

    First Commit

---
+++ firstprojectbatch/src/main/java/com/pegada/firstprojectbatch/FirstprojectbatchApplication.java
@@ -0,0 +1,14 @@
+package com.pegada.firstprojectbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class FirstprojectbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstprojectbatchApplication.class, args);
+	}
+
+}
---
+++ firstprojectbatch/src/test/java/com/pegada/firstprojectbatch/FirstprojectbatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pegada.firstprojectbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstprojectbatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
